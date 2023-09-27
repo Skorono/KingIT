5e1a63737f36a898ec1ea156054502eb6c5a9533
Mikhail Popov <astat.popov@yandex.ru>
2023-09-28 02:36:34 +0300

    .

---
+++ KingIT/Migrations/20230927225018_AddingDataMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KingIT.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingDataMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
