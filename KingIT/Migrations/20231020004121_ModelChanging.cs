f6f6c7ea0f624ed2e3503b006fbdc4eb90e42e52
Mikhail Popov <astat.popov@yandex.ru>
2023-10-20 03:44:57 +0300

    Миграции

---
+++ KingIT/Migrations/20231020004121_ModelChanging.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KingIT.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelChanging : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Number",
+                table: "Pavilions",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Pavilions",
+                newName: "Number");
+        }
+    }
+}
